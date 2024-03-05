70b4f8143ab54277422c0f2726b615579a8c2db4
Barrera-S-5921 <112194968+Barrera-S-5921@users.noreply.github.com>
2024-03-05 14:36:29 -0800

    yes
    yes

---
+++ Tik_Tac_Toe/Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int whoTurn; //0 = x and 1 = o
+    public int TurnCount; //counr the number of turns played
+    public GameObject[] turnicons; //displays whos turn it is
+    public Sprite[] playicons; //
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
