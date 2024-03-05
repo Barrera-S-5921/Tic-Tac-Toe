58a7366f0a374dcb3dc3467a46b5d3a8ae02b3ee
Barrera-S-5921 <112194968+Barrera-S-5921@users.noreply.github.com>
2024-03-05 15:25:40 -0800

    tyityutyu

--- Tik_Tac_Toe/Assets/Scripts/GameController.cs
+++ Tik_Tac_Toe/Assets/Scripts/GameController.cs
@@ -4,23 +4,55 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameController : MonoBehaviour
 {
-    public int whoTurn; //0 = x and 1 = o
-    public int TurnCount; //counr the number of turns played
+    public int whoseTurn; //0 = x and 1 = o
+    public int turnCount; //counr the number of turns played
     public GameObject[] turnicons; //displays whos turn it is
-    public Sprite[] playicons; //
+    public Sprite[] playerIcons; //0 = x icon and 1 = o icon
+    public Button[] tictactoespaces; //playable space for our game
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameSetup();
+    }
+
+    void GameSetup()
+    {
+        whoseTurn = 0;
+        turnCount = 0;
+        turnicons[0].SetActive(true);
+        turnicons[1].SetActive(false);
+        for(int i = 0; i <tictactoespaces.Length; i++)
+        {
+            tictactoespaces[i].interactable = true;
+            tictactoespaces[i].GetComponent<Image>().sprite = null;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void TicTacToeButton(int WhichNumber)
+    {
+        tictactoespaces[WhichNumber].image.sprite = playerIcons[whoseTurn];
+        tictactoespaces[WhichNumber].interactable = false;
+
+
+
+
+        if(whoseTurn == 0)
+        {
+            whoseTurn = 1;
+        }
+        else
+        {
+            whoseTurn = 0;
+        }
     }
 }
